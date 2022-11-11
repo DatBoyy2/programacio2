030c37973a2cbd061ae2862d971b212f77690d7c
DatBoyy2 <guilleme67@gmail.com>
2022-11-11 10:03:32 +0100

    Create exercici2.py

---
+++ exercici2.py
@@ -0,0 +1,18 @@
+iniciales=["charmander", "squirtle", "bulbasaur"]
+
+print(iniciales)
+
+juegos=("Pokemon Diamante", "Skyrim", "Ocarina Of Time", "Minecraft")
+print(juegos[0])
+print(juegos)
+
+persona={
+
+   "Nom": "Guillem",
+   "Edat":  "19 anys",
+   "Esport preferit": "No fer res",
+   "Enemic mortal": "Griffith de Berserk",
+
+}
+
+Pokemon=set("Cola Férrea", "Roca Afilada")
